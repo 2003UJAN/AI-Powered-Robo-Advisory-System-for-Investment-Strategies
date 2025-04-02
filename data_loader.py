c1131765c6a311107d9e5c83188dbeddd8d25cfe
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-04-02 13:09:42 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,14 @@
 import yfinance as yf
 import pandas as pd
 
-def get_stock_data(tickers, period="1y"):
-    stock_data = {ticker: yf.download(ticker, period=period)["Adj Close"] for ticker in tickers}
+def fetch_stock_data(stock_symbols, start_date="2020-01-01", end_date="2024-01-01"):
+    stock_data = {}
+    
+    for symbol in stock_symbols:
+        try:
+            stock = yf.download(symbol, start=start_date, end=end_date)
+            stock_data[symbol] = stock["Adj Close"]
+        except Exception as e:
+            print(f"Error fetching data for {symbol}: {e}")
+
     return pd.DataFrame(stock_data)
